955b98dc30dd35be91642af88801de76a5258718
gorqkop <gor.qkop@gmail.com>
2014-05-31 09:15:50 +0300

    Update script.py

--- script.py
+++ script.py
@@ -9,11 +9,12 @@ def collect_tweets(file_mask, terms, consumer_key, consumer_secret, access_token
     else: num = int(sorted(file, key=lambda x: int(x.split('_')[-1]))[-1].split('_')[-1])
     try:
         api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-        r = api.request('statuses/filter', {'track':terms})
         while 1==1:
+            r = api.request('statuses/filter', {'track':terms})
             for item in r.get_iterator():
                 if os.path.getsize('%s_%s'%(file_mask,str(num))) > 100000000: num += 1
                 with open('%s_%s'%(file_mask,str(num)), 'a', encoding='utf-8') as file: file.write('%s\n'%json.dumps(item, ensure_ascii=False))
     except:
         print('No internet connection! Reconnect in 10 seconds...')
         time.sleep(10)
+        collect_tweets(file_mask, terms, consumer_key, consumer_secret, access_token_key, access_token_secret)
